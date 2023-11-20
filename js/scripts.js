55f97e0e832a62333ce1791d17a1f3a8c515df63
behailu-2023 <142741099+behailu-2023@users.noreply.github.com>
2023-11-20 19:25:15 +0100

    update on the js through applying loops and conditionals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-let polemonList=[{name: "pikachu", height: "0.4", type: ["field","fairy"]}, {name: "krabby", height: "0.4", type: "water"}, {name: "combee", height: "0.3", type: "bug"}]
+const pokemonList = [
+    {name: 'Pikachu (height: 0.4)',
+     height: 0.4, 
+     type: ['field','fairy']},
+    {name: 'Krabby (height: 1)', 
+    height: 1,
+    type: 'water'}, 
+    {name: 'Combee (height: 0.3)', 
+    height: 0.3, 
+    type: 'bug'}
+];
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 0.5 && pokemonList[i].height > 0.3){
+    document.write(pokemonList[i].name);
+} 
+else if(pokemonList[i].height >= 1) 
+{
+    document.write(pokemonList[i].name + ' -Wow,that is big!');
+}else {
+document.write(pokemonList[i].name)
+}
+}
