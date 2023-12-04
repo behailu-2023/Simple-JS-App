e6d653b4433ee83279e19e6c6d6eaafb533a3ed3
behailu-2023 <142741099+behailu-2023@users.noreply.github.com>
2023-12-04 16:44:27 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-const pokemonList = [
+let pokemonRepository = (function () { 
+let pokemonList = [
     {name: 'Pikachu (height: 0.4)',
      height: 0.4, 
-     type: ['field','fairy']},
+     type: ['field','fairy']}, 
     {name: 'Krabby (height: 1)', 
     height: 1,
     type: 'water'}, 
@@ -9,7 +10,24 @@ const pokemonList = [
     height: 0.3, 
     type: 'bug'}
 ];
-for (let i = 0; i < pokemonList.length; i++) {
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+function getAll() {
+    return pokemonList;
+}
+return {
+    add: add,
+    getAll: getAll
+};
+
+})();
+console.log(pokemonRepository.getAll ());
+pokemonRepository.add({ name: 'Abra'});
+console.log(pokemonRepository.getAll());
+
+
+/*for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height < 0.5 && pokemonList[i].height > 0.3){
     document.write(pokemonList[i].name);
 } 
@@ -19,4 +37,13 @@ else if(pokemonList[i].height >= 1)
 }else {
 document.write(pokemonList[i].name)
 }
+}*/
+
+/*pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ' height ' + ' type ');
+    
+});*/
+function myFavoritePokemon(pokemon) {
+    console.log(pokemon.name + ' height ' + ' type ');
 }
+pokemonList.forEach(myFavoritePokemon);
