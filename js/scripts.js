6738a0a7fe8163b6f1484ea1ee2d0f2d0b4830f0
behailu-2023 <142741099+behailu-2023@users.noreply.github.com>
2023-12-21 14:44:25 +0100

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,75 @@
-let pokemonRepository = (function () {
-    let repository = [
+
+let pokemonRepository = (function () { 
+    let pokemonList = [
+        { name: 'Pikachu',
+        height: 0.4,
+        types: ['field', 'fairy'],},
         {
-            name: 'Pikachu (height: 0.4)',
-            height: 0.4,
-            types: ['field', 'fairy'],
-        },
-        {
-            name: 'Krabby (height: 1)',
+            name: 'Krabby',
             height: 1,
-            types: ['water'],
+            types: ['water'], 
         },
         {
-            name: 'Combee (height: 0.3)',
+            name: 'Combee',
             height: 0.3,
             types: ['bug'],
         }
+    
     ];
     function add(pokemon) {
-        if (
-            typeof pokemon === 'object' &&
-            'name' in pokemon &&
-            'height' in pokemon &&
-            'types' in pokemon
-        ) {
-            repository.push(pokemon);
+        if (typeof pokemon === 'object') { 
+        pokemonList.push(pokemon);
+      }
+    }
+    function getAll() {
+        return pokemonList;
+    }
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listitem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        listitem.appendChild(button);
+        pokemonList.appendChild(listitem);
+        button.addEventListener('click', () => {
+            showDetails(pokemon);
+        });
+    }
+    return{
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem
+    };
+    
+    })();
+    
+    //console.log(pokemonRepository.getAll());
+    pokemonRepository.add({name: 'Abra', height: 0.2, types: ['fire']});
+    console.log(pokemonRepository.getAll());
+    
+    pokemonRepository.getAll().forEach (function (pokemon){
+        /*if (pokemon.height > 0.4) {
+            document.write('<p>', pokemon.name + ' - ' + pokemon.height + ' Wow, that\'s big!; </p>');
         } else {
-            console.log('pokemon is not correct');
-        }
-        function getAll() {
-            return repository;
-        }
-        function addListItem(pokemon) {
-            let pokemonList = document.querySelector('.pokemon-list');
-            let listpokemon = document.createElement('li');
-            let button = document.createElement('button');
-            button.innerText = pokemon.name;
-            button.classList.add('button-class');
-            listpokemon.appendChild(button);
-            pokemonList.appendChild(listpokemon);
-        }
-        return {
-            add: add,
-            getAll: getAll,
-            addListItem: addListItem
-        };
-
-}
- }) ();
-
-    pokemonRepository.add({ name: 'Abra', height: 0.9, type: ['psychic'] });
-    document.write(pokemonRepository.getAll());
-
-    pokemonRepository.getAll().forEach(function (pokemon) {
+            document.write('<p>', pokemon.name + ' - ' + pokemon.height +   '</p>');
+        }*/
         pokemonRepository.addListItem(pokemon);
-});
+    });
+    
+    
+    
+        
+    
+    
+    
+    
+    
+
+
+
+
+
